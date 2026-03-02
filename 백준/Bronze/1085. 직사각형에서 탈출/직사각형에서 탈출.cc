c42ff67402eb8bf79f92f0a297d38ce24c3f90e5
hwisu03 <145253832+hwisu03@users.noreply.github.com>
2026-03-02 12:22:56 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -1,21 +1,14 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
-int main(){
-    int x,y,w,h;
+
+int main() {
+    int x, y, w, h;
     cin >> x >> y >> w >> h;
 
-    int dist = 9999;
-    if (x<dist){
-        dist = x;
-    }
-    if (y<dist){
-        dist = y;
-    }
-    if (w-x<dist){
-        dist = w-x;
-    }
-    if(h-y<dist){
-        dist = h-y;
-    }
-cout << dist;
+    
+    cout << min({x, y, w - x, h - y}) << "\n";
+
+    return 0;
 }
